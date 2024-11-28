e3fd64665e7efdabd81607e60dfd41440ca0e37d
בועז אלון <boaz@MacBook-Air-sl-bwz.local>
2024-11-28 17:33:20 +0200

    4

--- Anagram.java
+++ Anagram.java
@@ -100,18 +100,18 @@ public class Anagram {
 	public static String randomAnagram(String str) {
 		// Replace the following statement with your code
 		String newstr = "";
-		double radnom = Math.random() * str.length();
+		double random = Math.random() * str.length();
 		for (int i = 0; i < str.length(); i++) {
-			if (str.charAt((int) radnom) != ' ') {
-				newstr += str.charAt((int) radnom);
-				str = str.replace(str.charAt((int) radnom), ' ');
+			if (str.charAt((int) random) != ' ') {
+				newstr += str.charAt((int) random);
+				str = str.replace(str.charAt((int) random), ' ');
 			} else {
-				while (str.charAt((int) radnom) == ' ') {
-					radnom = Math.random() * str.length();
+				while (str.charAt((int) random) == ' ') {
+					random = Math.random() * str.length();
 				}
-				newstr += str.charAt((int) radnom);
+				newstr += str.charAt((int) random);
 			}
-			radnom = Math.random() * str.length();
+			random = Math.random() * str.length();
 		}
 
 		return newstr;
